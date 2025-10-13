095cc276b64b4dd038a9fb40cc6d666fbcfe52f0
Nagendra Dwivedi <107978999+Nagendra987@users.noreply.github.com>
2025-10-13 12:23:46 +0000

    Operations

--- conversionAndOperation.js
+++ conversionAndOperation.js
@@ -27,11 +27,50 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 let someNumber =  17
 let stringSomeNumber = String(someNumber);
 
-console.log(stringSomeNumber);
-console.log(typeof(stringSomeNumber));
+// console.log(stringSomeNumber);
+// console.log(typeof(stringSomeNumber));
 
 
+// ************************* Operations *************************
 
+let value = 3;
+let negValue = -value;
+
+// console.log(negValue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = 'Hello'
+let str2 = ' Nagendra'
+ let str3 = str1 + str2
+// console.log(str3)
+
+// console.log(1+"2");
+// console.log("1"+"2");
+// console.log("1"+2);
+// console.log("1"+2+3);
+// console.log(1+2+'3')
+
+// console.log((3 + 4) * 5 % 3)
+
+// console.log(true)
+// console.log(+true)
+// console.log(+'')
+
+
+let num1, null2, num3;
+
+num1 = num2 = num3 = 2 + 3;
+console.log(num1); 
+
+let gameCounter = 100;
+// gameCounter++;
+++gameCounter;
+console.log(gameCounter);
 // // ---------- Implicit Conversion ----------
 // console.log("\n-- Implicit Conversion --");
 // console.log("'5' + 2 =", '5' + 2);   // "52" → number converted to string
