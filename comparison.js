9838db3491a4d44a4e2518542bffc71a1467e0b0
Nagendra Dwivedi <107978999+Nagendra987@users.noreply.github.com>
2025-10-13 12:38:06 +0000

    comparison.js

---
+++ comparison.js
@@ -0,0 +1,17 @@
+// console.log('comparison.js');
+
+// console.log(2 > 4)
+// console.log(2 >= 4)
+// console.log(2 < 4)
+// console.log(2 == 4)
+// console.log(2 != 4)
+
+// console.log(null > 0)
+// console.log(null < 0)
+// console.log(null == 0)
+// console.log(null >= 0)
+
+// console.log(undefined > 0)
+// console.log(undefined < 0)
+// console.log(undefined == 0)
+// console.log(undefined >= 0)
